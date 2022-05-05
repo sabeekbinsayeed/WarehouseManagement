0d13750aa88d6f53c856cc72788cca931941d63a
sabeekbinsayeed <sabikbinsayeed@gmail.com>
2022-05-06 02:43:11 +0600

    delete-product

--- src/components/AllProduct/AllProduct.js
+++ src/components/AllProduct/AllProduct.js
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Table } from 'react-bootstrap';
 
 const AllProduct = props => {
     const { name, price, img, quantity, supplier, _id } = props.product;
+    const [products, setProducts] = useState([])
+    useEffect(() => {
+        fetch('http://localhost:5000/products').then(res => res.json()).then(data => setProducts(data))
+    }, [])
+    const handleDelete = id => {
+        const proceed = window.confirm('sure?');
+        if (proceed) {
+            console.log('deleting user with id, ', id);
+            const url = `http://localhost:5000/products/${id}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deletedCount > 0) {
+                        console.log('deleted');
+                        const remaining = products.filter(u => u._id !== id);
+                        setProducts(remaining);
+                    }
+                })
+        }
+        else {
+            console.log('else ')
+        }
+    }
     return (
         <tr>
             <td>{name}</td>
             <td>{quantity}</td>
             <td>{supplier}</td>
-            <td><button>delete</button></td>
+            <td><button onClick={() => handleDelete(_id)}>delete</button></td>
 
         </tr>
     );
--- src/components/ManageInventory/ManageInventory.js
+++ src/components/ManageInventory/ManageInventory.js
@@ -7,6 +7,28 @@ const ManageInventory = () => {
     useEffect(() => {
         fetch('http://localhost:5000/products').then(res => res.json()).then(data => setProducts(data))
     }, [])
+
+    const handleDelete = id => {
+        const proceed = window.confirm('sure?');
+        if (proceed) {
+            console.log('deleting user with id, ', id);
+            const url = `http://localhost:5000/products/${id}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deletedCount > 0) {
+                        console.log('deleted');
+                        const remaining = products.filter(u => u._id !== id);
+                        setProducts(remaining);
+                    }
+                })
+        }
+        else {
+            console.log('else ')
+        }
+    }
     return (
         <div>
             <Table striped bordered hover>
@@ -22,8 +44,18 @@ const ManageInventory = () => {
                 <tbody>
 
                     {
-                        products.map(product => <AllProduct key={product._id} product={product}></AllProduct>)
+                        products.map(product => <tr>
+                            <td>{product.name}</td>
+                            <td>{product.quantity}</td>
+                            <td>{product.supplier}</td>
+                            <td><button onClick={() => handleDelete(product._id)}>delete</button></td>
+
+                        </tr>)
                     }
+
+                    {/* {
+                        products.map(product => <AllProduct key={product._id} product={product}></AllProduct>)
+                    } */}
 
 
 
