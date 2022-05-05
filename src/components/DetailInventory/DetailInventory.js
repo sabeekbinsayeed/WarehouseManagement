528e507a7a837d250a10411694a040a2ffee5649
sabeekbinsayeed <sabikbinsayeed@gmail.com>
2022-05-05 07:04:03 +0600

    detail-inventory-modified

--- src/components/DetailInventory/DetailInventory.js
+++ src/components/DetailInventory/DetailInventory.js
@@ -1,10 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import { Button } from 'react-bootstrap';
+import React, { useEffect, useRef, useState } from 'react';
+import { Form } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 
 const DetailInventory = () => {
     const { productId } = useParams();
     const [pdetail, setPdetail] = useState([])
-    const [quantity, setQuantity] = useState(0)
+    const [quantity, setQuantity] = useState(0);
+    const stockRef = useRef('')
 
     useEffect(() => {
         const url = `http://localhost:5000/products/${productId}`
@@ -23,12 +26,16 @@ const DetailInventory = () => {
         console.log('clicked')
 
         const quantity = detail.pdetail.quantity - 1;
-        const name = detail.pdetail.name
 
-        const updatedProduct = { name, quantity };
+        const updatedProduct = { quantity };
         console.log(updatedProduct)
+        handleNumber(updatedProduct, quantity)
 
         // send data to the server
+
+    }
+
+    const handleNumber = (updatedProducts, quantity) => {
         const url = `http://localhost:5000/products/${pdetail._id}`;
         console.log(url)
         fetch(url, {
@@ -36,16 +43,31 @@ const DetailInventory = () => {
             headers: {
                 'content-type': 'application/json'
             },
-            body: JSON.stringify(updatedProduct)
+            body: JSON.stringify(updatedProducts)
         })
             .then(res => res.json())
             .then(data => {
                 console.log('success', data);
                 alert('users added successfully!!!');
                 setQuantity(quantity)
+                console.log(quantity)
                 pdetail.quantity = quantity;
 
             })
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        const stock = stockRef.current.value;
+        const quantity = stock
+        //  const name = detail.pdetail.name
+
+        const updatedProduct = { quantity };
+        console.log(updatedProduct)
+        handleNumber(updatedProduct, quantity)
+
+        console.log(stock)
+
     }
     return (
         <div>
@@ -54,6 +76,22 @@ const DetailInventory = () => {
             <p>{quantity}</p>
 
             <button onClick={() => handleUpdate({ pdetail })}>Delivered</button>
+
+            <Form onSubmit={handleSubmit}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Input stock</Form.Label>
+                    <Form.Control ref={stockRef} type="text" placeholder="Enter stock" />
+
+                </Form.Group>
+
+
+                {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group> */}
+                <Button variant="primary w-50 mx-auto d-block mb-2" type="submit">
+                    Login
+                </Button>
+            </Form>
         </div>
     );
 };
